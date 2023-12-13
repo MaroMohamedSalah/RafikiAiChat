cde3091f599966680899af7b6ed85fe24db7c7a9
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-12-13 17:33:00 +0200

    update rule based

--- new.py
+++ new.py
@@ -1,42 +1,60 @@
 import json
 import random
+import Levenshtein
 
 from nltk import pos_tag, word_tokenize
 from nltk.corpus import stopwords
 from contractions import fix
 from Chat_Bot import detect_language
+from nltk.stem import WordNetLemmatizer
 
-def load_intents_from_json(file_path):
-    with open(file_path, 'r',encoding='utf-8') as file:
-        data = json.load(file)
-    return data['intents']
+def handle_input(input_string):
+    result = ""
+    for char in input_string:
+        if len(result) == 0 or char != result[-1]:
+            result += char
+    return result
+
+def load_intents_from_json(file_paths):
+    all_intents = []
+    for file_path in file_paths:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        all_intents.extend(data.get('intents', []))
+    return all_intents
+
+def process_input(input_text):
+    processed_input = handle_input(input_text.lower().replace(" ", ""))
+    return processed_input
 
 def tokenize_user_input(user_input_str):
     lang = 'english'
     if detect_language(user_input_str) == 'ar':
         lang = "arabic"
 
-    # Handle contractions
     fixed_input = fix(user_input_str)
 
-    # If fix returns a tuple, extract the fixed string
     if isinstance(fixed_input, tuple):
         fixed_input = fixed_input[0]
 
-    # Tokenization Input
     tokens = word_tokenize(fixed_input.lower())
     pos_tags = pos_tag(tokens)
 
-    # Remove stopwords
     stop_words = set(stopwords.words(lang))
     tokens = [token for token in tokens if token not in stop_words]
 
     return tokens
 
 def get_response(intent, user_input_str):
+    processed_input = process_input(user_input_str)
+
     for pattern in intent['patterns']:
-        if pattern.lower() in user_input_str.lower():
-            return random.choice(intent['responses'])
+        processed_keyword = process_input(pattern)
+        if processed_input == processed_keyword:
+            similarity = 1 - Levenshtein.distance(processed_input, processed_keyword) / max(len(processed_input), len(processed_keyword))
+            if similarity >= 0.8:
+                return random.choice(intent['responses'])
+
     return None
 
 def chatbot(intents):
@@ -62,8 +80,7 @@ def chatbot(intents):
         else:
             print("Chatbot: I'm sorry, I didn't understand that. Can you please rephrase?")
 
-
 # Main program
-json_file_path = 'data_sets/combine.json'  # Update this with your JSON file path
-intents = load_intents_from_json(json_file_path)
+json_file_paths = ['data_sets/jocks-datasets.json', 'data_sets/college_info.json', 'data_sets/general-dataset.json','data_sets/rafiki-info-dataset.json','data_sets/student-assistant-dataset.json']
+intents = load_intents_from_json(json_file_paths)
 chatbot(intents)
