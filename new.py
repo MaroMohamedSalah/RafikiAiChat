a6fc2a12a302174730a0ea23ceef1e15bcfdb2aa
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-12-16 16:12:01 +0200

    file structure enhance: add response_generator.py

--- new.py
+++ new.py
@@ -1,41 +1,6 @@
-import random
-
-from fuzzy_matcher import fuzzy_match
-from input_processor import process_input, tokenize_user_input
+from input_processor import tokenize_user_input
 from intent_loader import load_intents_from_json
-
-
-def get_response(intent, user_input_str):
-    processed_input = process_input(user_input_str).lower()
-    processed_patterns = list(map(lambda pattern: process_input(pattern).lower(), intent['patterns']))
-
-    # Exact match check with the same length
-    exact_matches = [pattern for pattern in processed_patterns if
-                     len(processed_input) == len(pattern) and processed_input == pattern]
-    if exact_matches:
-        return random.choice(intent['responses'])
-
-    # Check if input length is less than 3 characters or contains less than 2 tokens, then return no response
-    if len(processed_input) < 3 or len(tokenize_user_input(user_input_str)) < 2:
-        return None
-
-    # Fuzzy match check with a higher threshold for input length >= 3
-    fuzzy_threshold_high = 90
-    fuzzy_high_matches = fuzzy_match(processed_input, intent['patterns'], fuzzy_threshold_high)
-    if fuzzy_high_matches:
-        return random.choice(intent['responses'])
-
-    # Fuzzy match check with a lower threshold for input length >= 3
-    fuzzy_threshold_low = 65
-    fuzzy_low_matches = fuzzy_match(processed_input, intent['patterns'], fuzzy_threshold_low)
-    if fuzzy_low_matches:
-        return random.choice(intent['responses'])
-
-    # If no direct or fuzzy match is found, check if any pattern is present in the processed input
-    if any(pattern.lower() in processed_input for pattern in intent['patterns']):
-        return intent.get("response", "I don't understand.")
-
-    return None
+from response_generator import generate_response
 
 
 def chatbot(intents):
@@ -52,7 +17,7 @@ def chatbot(intents):
 
         response = None
         for intent in intents:
-            response = get_response(intent, user_input_str)
+            response = generate_response(intent, user_input_str)
 
             if response:
                 break
---
+++ response_generator.py
@@ -0,0 +1,45 @@
+import random
+from fuzzy_matcher import fuzzy_match
+from input_processor import process_input, tokenize_user_input
+
+
+def generate_response(intent, user_input_str):
+    """
+    Generates a response based on the provided intent and user input.
+
+    Args:
+    - intent (dict): The intent object containing patterns and responses.
+    - user_input_str (str): The user's input string.
+
+    Returns:
+    - response (str): The generated response based on matching patterns.
+    """
+    processed_input = process_input(user_input_str).lower()
+    processed_patterns = list(map(lambda pattern: process_input(pattern).lower(), intent['patterns']))
+
+    # Exact match check with the same length
+    exact_matches = [pattern for pattern in processed_patterns if len(processed_input) == len(pattern) and processed_input == pattern]
+    if exact_matches:
+        return random.choice(intent['responses'])
+
+    # Check if input length is less than 3 characters or contains less than 2 tokens, then return no response
+    if len(processed_input) < 3 or len(tokenize_user_input(user_input_str)) < 2:
+        return None
+
+    # Fuzzy match check with a higher threshold for input length >= 3
+    fuzzy_threshold_high = 90
+    fuzzy_high_matches = fuzzy_match(processed_input, intent['patterns'], fuzzy_threshold_high)
+    if fuzzy_high_matches:
+        return random.choice(intent['responses'])
+
+    # Fuzzy match check with a lower threshold for input length >= 3
+    fuzzy_threshold_low = 65
+    fuzzy_low_matches = fuzzy_match(processed_input, intent['patterns'], fuzzy_threshold_low)
+    if fuzzy_low_matches:
+        return random.choice(intent['responses'])
+
+    # If no direct or fuzzy match is found, check if any pattern is present in the processed input
+    if any(pattern.lower() in processed_input for pattern in intent['patterns']):
+        return intent.get("response", "I don't understand.")
+
+    return None
