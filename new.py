4a06921f5e0f1e27f33e477e0af4392de5172b3d
MariaAbdallah1 <151842825+MariaAbdallah1@users.noreply.github.com>
2023-12-16 16:26:12 +0200

    updates to get user name

--- new.py
+++ new.py
@@ -1,6 +1,7 @@
 from input_processor import tokenize_user_input
 from intent_loader import load_intents_from_json
 from response_generator import generate_response
+from user_name import extract_user_name
 
 
 def chatbot(intents):
@@ -14,7 +15,12 @@ def chatbot(intents):
         if user_input_str.lower() == 'exit':
             print("Chatbot: Goodbye!")
             break
-
+            
+        user_name = extract_user_name(user_input_str)
+        if user_name:
+            print(f"rafikak: Hi {user_name}! How can I assist you today?")
+            continue
+            
         response = None
         for intent in intents:
             response = generate_response(intent, user_input_str)
