071d9c4d4b7404b22b017accf08921830d07726c
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-12-16 15:44:27 +0200

    file structure enhance: add fuzzy_matcher.py

--- new.py
+++ new.py
@@ -7,12 +7,16 @@ from Chat_Bot import detect_language
 from fuzzywuzzy import fuzz
 from nltk.stem import WordNetLemmatizer
 
+from fuzzy_matcher import fuzzy_match
+
+
 def handle_input(input_string):
     result = ""
     for char in input_string:
         if len(result) == 0 or char != result[-1]:
             result += char
     return result
+
 
 def load_intents_from_json(file_paths):
     all_intents = []
@@ -22,9 +26,11 @@ def load_intents_from_json(file_paths):
         all_intents.extend(data.get('intents', []))
     return all_intents
 
+
 def process_input(input_text):
     processed_input = handle_input(input_text.lower())
     return processed_input
+
 
 def tokenize_user_input(user_input_str):
     lang = 'english'
@@ -36,35 +42,34 @@ def tokenize_user_input(user_input_str):
 
     pos_tags = pos_tag(tokens)
 
-    #stop_words = set(stopwords.words(lang))
-  #  tokens = [token for token in tokens if token not in stop_words]
+    # stop_words = set(stopwords.words(lang))
+    #  tokens = [token for token in tokens if token not in stop_words]
 
     return tokens
 
+
 def get_response(intent, user_input_str):
     processed_input = process_input(user_input_str).lower()
-
-    # Debugging exact match check
-    # print("Processed Input:", processed_input)
     processed_patterns = list(map(lambda pattern: process_input(pattern).lower(), intent['patterns']))
-    # print("Processed Patterns:", processed_patterns)
 
     # Exact match check with the same length
     exact_matches = [pattern for pattern in processed_patterns if len(processed_input) == len(pattern) and processed_input == pattern]
     if exact_matches:
         return random.choice(intent['responses'])
 
-    # Check if input length is less than 3 characters, then return no response
-    if len(processed_input) < 3:
+    # Check if input length is less than 3 characters or contains less than 2 tokens, then return no response
+    if len(processed_input) < 3 or len(tokenize_user_input(user_input_str)) < 2:
         return None
 
     # Fuzzy match check with a higher threshold for input length >= 3
-    fuzzy_high_matches = [pattern for pattern in intent['patterns'] if fuzz.partial_ratio(processed_input, process_input(pattern).lower()) >= 90 and len(processed_input) == len(pattern)]
+    fuzzy_threshold_high = 90
+    fuzzy_high_matches = fuzzy_match(processed_input, intent['patterns'], fuzzy_threshold_high)
     if fuzzy_high_matches:
         return random.choice(intent['responses'])
 
     # Fuzzy match check with a lower threshold for input length >= 3
-    fuzzy_low_matches = [pattern for pattern in intent['patterns'] if fuzz.partial_ratio(processed_input, process_input(pattern).lower()) >= 65 and len(processed_input) == len(pattern)]
+    fuzzy_threshold_low = 65
+    fuzzy_low_matches = fuzzy_match(processed_input, intent['patterns'], fuzzy_threshold_low)
     if fuzzy_low_matches:
         return random.choice(intent['responses'])
 
@@ -73,6 +78,7 @@ def get_response(intent, user_input_str):
         return intent.get("response", "I don't understand.")
 
     return None
+
 
 
 def chatbot(intents):
@@ -99,8 +105,9 @@ def chatbot(intents):
         else:
             print("Chatbot: I'm sorry, I didn't understand that. Can you please rephrase?")
 
+
 # Main program
-json_file_paths = ['data_sets/college_info.json','data_sets/jocks-datasets.json',  'data_sets/general-dataset.json','data_sets/rafiki-info-dataset.json','data_sets/student-assistant-dataset.json']
+json_file_paths = ['data_sets/college_info.json', 'data_sets/jocks-datasets.json', 'data_sets/general-dataset.json',
+                   'data_sets/rafiki-info-dataset.json', 'data_sets/student-assistant-dataset.json']
 intents = load_intents_from_json(json_file_paths)
 chatbot(intents)
-
