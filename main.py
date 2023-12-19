45fa9c1de6bc5f0ddc48711ffd86a476c42f858f
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-12-19 12:11:36 +0200

    enhance rafiki-info-dataset.json

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from components.intent_loader import load_intents_from_json
 
 json_file_paths = ['data_sets/college_info.json',
                    'data_sets/jocks-datasets.json',
-                   'data_sets/general-dataset.json',
+                   # 'data_sets/general-dataset.json',
                    'data_sets/rafiki-info-dataset.json',
                    'data_sets/student-assistant-dataset.json']
 
